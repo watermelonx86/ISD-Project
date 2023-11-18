02c46bded8d5519bf8cac13eb7095a34b2e642b4
Long Huỳnh <watermelonX86@outlook.com>
2023-11-18 21:17:29 +0700

    add necessary packages and create connection to postgresql

---
+++ ISD-Project.Server/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ISD_Project.Server.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+        
+        //Constructor
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+    }
+}
