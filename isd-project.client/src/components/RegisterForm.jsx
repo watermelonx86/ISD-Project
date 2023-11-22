ac866bb366e378998dd56894d0b7e06bdb50a40c
tdkunz <iloverosebp1102@gmail.com>
2023-11-22 21:39:45 +0700

    FE-Login-Signup

--- isd-project.client/src/components/RegisterForm.jsx
+++ isd-project.client/src/components/RegisterForm.jsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
+﻿import { useState } from "react";
 import axios from "axios";
+import { useLocation, NavLink } from 'react-router-dom';
+
+
 const RegisterForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -24,7 +27,7 @@ const RegisterForm = () => {
         })
     }
 
-    return (
+    /*return (
     <div>
         <h1>Register Form</h1>
         <form>
@@ -46,7 +49,94 @@ const RegisterForm = () => {
             <button type="button" onClick={handleRegister}>Register</button>
         </form>
     </div>
-    );
+    );*/
+    return (
+        <section className="bg-gray-50">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <NavLink to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-black">
+                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                        Trang chủ
+                </NavLink>
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Create and account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                                    Your email
+                                </label>
+                                <input type="email"
+                                    name="email"
+                                    id="email"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="name@gmail.com"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                                    Password
+                                </label>
+                                <input type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required />
+                            </div>
+                            <div>
+                                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                                    Confirm password
+                                </label>
+                                <input type="password"
+                                    name="confirm-password"
+                                    id="confirm-password"
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    required />
+                            </div>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="terms"
+                                        aria-describedby="terms"
+                                        type="checkbox"
+                                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                        required />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
+                                        I accept the
+                                        <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">
+                                            Terms and Conditions
+                                        </a>
+                                    </label>
+                                </div>
+                            </div>
+                            <button type="submit"
+                                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                onClick={handleRegister}
+                            >
+                                Create an account
+                            </button>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Already have an account?
+                                <NavLink to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                                    Login here
+                                </NavLink>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default RegisterForm;
--- isd-project.client/src/main.jsx
+++ isd-project.client/src/main.jsx
@@ -5,10 +5,21 @@ import './index.css'
 import LoginForm from './components/LoginForm.jsx'
 import RegisterForm from './components/RegisterForm.jsx'
 
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+
+import App from './App'
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-        {/* <App /> */}
-        <LoginForm />
-        <RegisterForm />
-  </React.StrictMode>,
+    <Router>
+        <Routes>
+            <Route path='/' element={<App />} />
+            <Route path='/login' element={<LoginForm />} />
+            <Route path='/signup' element={<RegisterForm />} />
+        </Routes>
+        {/*{<LoginForm />
+        <RegisterForm />}*/}
+    </Router>
 )
+
