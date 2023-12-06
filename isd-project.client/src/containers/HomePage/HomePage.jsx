c23adffd088684c1551d2d83aa606ba539fbfa69
datk1912 <datk1912@gmail.com>
2023-12-06 16:21:25 +0700

    Update HomePage.jsx

--- isd-project.client/src/containers/HomePage/HomePage.jsx
+++ isd-project.client/src/containers/HomePage/HomePage.jsx
@@ -5,65 +5,68 @@ import bhsksp from "../../assets/bhsksp.jpeg"
 
 const HomePage = () => {
     return (
-        <section className="font-[Inter] bg-slate-100">
+        <div className="bg-slate-100">
             <Header />
-            <div className=" w-full h-650 mb-10">
-                <img src={bhsk} className="w-full h-650 blur z-0 absolute"/>
-                <h1 className="flex items-center justify-center w-full h-full z-20 relative text-5xl">
-                    Bao hiem suc khoe
+            <section className="font-[Inter]">
+
+                <div className=" w-full h-650 mb-10">
+                    <img src={bhsk} className="w-full h-650 blur z-0 absolute"/>
+                    <h1 className="flex items-center justify-center w-full h-full z-20 relative text-5xl">
+                        Bao hiem suc khoe
+                    </h1>
+                </div>
+
+                <h1 className="flex items-center justify-center w-full h-full text-5xl py-10">
+                    Cac goi bao hiem
                 </h1>
-            </div>
+                <div class="w-3/4 grid grid-cols-3 gap-20 mx-auto ">
+                    <div className="bg-white h-510 rounded p-5 shadow-xl shadow-slate-300 hover:shadow-slate-400 duration-300 cursor-pointer ">
+                        <img src={bhsksp} className="w-full h-2/4 rounded"/>
+                        <p className="pt-3">
+                            11/12/2023
+                        </p>
+                        <h2 className="flex item-center justify-center w-full text-4xl">
+                            Goi bao hiem gi do
+                        </h2>
+                        <p className="">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam eveniet laborum aut fugiat repellendus ad ducimus. Id odio voluptate, impedit iusto eius facere doloremque, reprehenderit tempora doloribus aspernatur iure.
+                        </p>
+                    </div>
 
-            <h1 className="flex items-center justify-center w-full h-full text-5xl py-10">
-                Cac goi bao hiem
-            </h1>
-            <div class="w-3/4 grid grid-cols-3 gap-20 mx-auto ">
-                <div className="bg-white h-510 rounded p-5 shadow-xl shadow-slate-300 hover:shadow-slate-400 duration-300 cursor-pointer ">
-                    <img src={bhsksp} className="w-full h-2/4 rounded"/>
-                    <p className="pt-3">
-                        11/12/2023
-                    </p>
-                    <h2 className="flex item-center justify-center w-full text-4xl">
-                        Goi bao hiem gi do
-                    </h2>
-                    <p className="">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam eveniet laborum aut fugiat repellendus ad ducimus. Id odio voluptate, impedit iusto eius facere doloremque, reprehenderit tempora doloribus aspernatur iure.
-                    </p>
+                    <div className="bg-white h-510 rounded p-5 shadow-xl shadow-slate-300 hover:shadow-slate-400 duration-300 cursor-pointer">
+                        <img src={bhsksp} className="w-full h-2/4 rounded"/>
+                        <p className="pt-3">
+                            11/12/2023
+                        </p>
+                        <h2 className="flex item-center justify-center w-full text-4xl">
+                            Goi bao hiem gi do
+                        </h2>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam eveniet laborum aut fugiat repellendus ad ducimus. Id odio voluptate, impedit iusto eius facere doloremque, reprehenderit tempora doloribus aspernatur iure.
+                        </p>
+                    </div>
+
+                    <div className="bg-white h-510 rounded p-5 shadow-xl shadow-slate-300 hover:shadow-slate-400 duration-300 cursor-pointer">
+                        <img src={bhsksp} className="w-full h-2/4 rounded"/>
+                        <p className="pt-3">
+                            11/12/2023
+                        </p>
+                        <h2 className="flex item-center justify-center w-full text-4xl">
+                            Goi bao hiem gi do
+                        </h2>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam eveniet laborum aut fugiat repellendus ad ducimus. Id odio voluptate, impedit iusto eius facere doloremque, reprehenderit tempora doloribus aspernatur iure.
+                        </p>
+                    </div>
+
                 </div>
-
-                <div className="bg-white h-510 rounded p-5 shadow-xl shadow-slate-300 hover:shadow-slate-400 duration-300 cursor-pointer">
-                    <img src={bhsksp} className="w-full h-2/4 rounded"/>
-                    <p className="pt-3">
-                        11/12/2023
-                    </p>
-                    <h2 className="flex item-center justify-center w-full text-4xl">
-                        Goi bao hiem gi do
-                    </h2>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam eveniet laborum aut fugiat repellendus ad ducimus. Id odio voluptate, impedit iusto eius facere doloremque, reprehenderit tempora doloribus aspernatur iure.
-                    </p>
-                </div>
-
-                <div className="bg-white h-510 rounded p-5 shadow-xl shadow-slate-300 hover:shadow-slate-400 duration-300 cursor-pointer">
-                    <img src={bhsksp} className="w-full h-2/4 rounded"/>
-                    <p className="pt-3">
-                        11/12/2023
-                    </p>
-                    <h2 className="flex item-center justify-center w-full text-4xl">
-                        Goi bao hiem gi do
-                    </h2>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio aliquam eveniet laborum aut fugiat repellendus ad ducimus. Id odio voluptate, impedit iusto eius facere doloremque, reprehenderit tempora doloribus aspernatur iure.
-                    </p>
-                </div>
-
-            </div>
-            <button className="bg-red-500 mx-auto flex item-center justify-center mt-20 px-5 py-3 text-2xl rounded-3xl text-white shadow-lg shadow-slate-400 
-                                hover:bg-red-700 active:bg-red-700 active:shadow-lg duration-300 mb-10">
-                    Xem them
-            </button>
+                <button className="bg-red-500 mx-auto flex item-center justify-center mt-20 px-5 py-3 text-2xl rounded-3xl text-white shadow-lg shadow-slate-400 
+                                    hover:bg-red-700 active:bg-red-700 active:shadow-lg duration-300 mb-10">
+                        Xem them
+                </button>
+            </section>
             <Footer />
-        </section>
+        </div>
     )
 }
 
